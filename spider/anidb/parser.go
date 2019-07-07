21859c8ced534b5531b1dc2a4ef4992751c53e99
Igor Nikitin <rabbitinspace@icloud.com>
2019-07-07 20:15:58 +1000

    fix regex groups and improve logs

--- spider/anidb/parser.go
+++ spider/anidb/parser.go
@@ -12,7 +12,7 @@ import (
 	"satelit-project/satelit-scraper/proto/scraper"
 
 	"github.com/PuerkitoBio/goquery"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 type Error struct {
@@ -26,6 +26,7 @@ func (e *Error) Error() string {
 type Parser struct {
 	url *url.URL
 	doc *goquery.Document
+	log *logrus.Entry
 }
 
 func NewParser(url *url.URL, html io.Reader) (*Parser, error) {
@@ -33,9 +34,36 @@ func NewParser(url *url.URL, html io.Reader) (*Parser, error) {
 	if err != nil {
 		return nil, err
 	}
+	
+	fields := logrus.Fields{"db": "anidb"}
+	if id, err := parseSource(url.String(), "aid="); err == nil {
+		fields["id"] = id
+	}
 
-	p := Parser{url, doc}
+	p := Parser{url, doc, logrus.WithFields(fields)}
 	return &p, nil
+}
+
+func (p *Parser) Anime() (*scraper.Anime, error) {
+	anime := scraper.Anime{
+		Source:               p.Source(),
+		Type:                 p.Type(),
+		Title:                p.Title(),
+		PosterUrl:            p.PosterURL(),
+		EpisodesCount:        p.EpisodesCount(),
+		Episodes:             p.Episodes(),
+		StartDate:            p.StartDate(),
+		EndDate:              p.EndDate(),
+		Tags:                 p.Tags(),
+		Rating:               p.Rating(),
+		Description:          p.Description(),
+	}
+
+	if anime.Source == nil {
+		return nil, &Error{"will not create Anime because Source is not valid"}
+	}
+
+	return &anime, nil
 }
 
 func (p *Parser) Source() *scraper.Anime_Source {
@@ -43,7 +71,7 @@ func (p *Parser) Source() *scraper.Anime_Source {
 
 	id, err := parseSource(p.url.String(), "aid=")
 	if err != nil {
-		log.Errorf("anidb id is malformed: %v", err)
+		p.log.Warnf("anidb id is malformed: %v", err)
 		return nil
 	}
 
@@ -52,7 +80,7 @@ func (p *Parser) Source() *scraper.Anime_Source {
 	p.doc.Find(`div.g_definitionlist tr.resources a[href*="myanimelist"]`).Each(func(_ int, s *goquery.Selection) {
 		id, err := parseSource(s.AttrOr("href", ""), "/")
 		if err != nil {
-			log.Errorf("mal id is malformed: %v", err)
+			p.log.Warnf("mal id is malformed: %v", err)
 		}
 
 		source.MalId = append(source.MalId, id)
@@ -61,7 +89,7 @@ func (p *Parser) Source() *scraper.Anime_Source {
 	p.doc.Find(`div.g_definitionlist tr.resources a[href*="animenewsnetwork"]`).Each(func(_ int, s *goquery.Selection) {
 		id, err := parseSource(s.AttrOr("href", ""), "id=")
 		if err != nil {
-			log.Errorf("ann id is malformed: %v", err)
+			p.log.Warnf("ann id is malformed: %v", err)
 		}
 
 		source.AnnId = append(source.AnnId, id)
@@ -111,7 +139,7 @@ func (p *Parser) EpisodesCount() int32 {
 	if ep, err := strconv.Atoi(prop); err == nil && len(prop) > 0 {
 		return int32(ep)
 	} else if err != nil {
-		log.Errorf("failed to parse itemprop=\"numberOfEpisodes\"]: %v", err)
+		p.log.Warnf("failed to parse itemprop=\"numberOfEpisodes\"]: %v", err)
 	}
 
 	// try to parse from row's text
@@ -120,13 +148,13 @@ func (p *Parser) EpisodesCount() int32 {
 
 	// number after comma, usually for TV type
 	match := regexp.MustCompile(`,\s*(\d+)`).FindStringSubmatch(raw)
-	if len(match) > 1 {
-		log.Warnf("EpisodesCount() regexp found multiple ep numbers: %v", match)
-	} else if len(match) == 1 {
-		if ep, err := strconv.Atoi(match[0]); err == nil {
+	if len(match) == 0 {
+		p.log.Warnf("didn't find number of episodes for: %v", raw)
+	} else {
+		if ep, err := strconv.Atoi(match[1]); err == nil {
 			return int32(ep)
 		} else {
-			log.Errorf("failed to parse episodes count: %v", err)
+			p.log.Warnf("failed to parse episodes count: %v", err)
 		}
 	}
 
@@ -139,28 +167,49 @@ func (p *Parser) EpisodesCount() int32 {
 	// probably has comma and some text but no numbers, usually number of ep is unknown
 	match = regexp.MustCompile(`^\D+,?([^\d])+$`).FindStringSubmatch(raw)
 	if len(match) > 0 {
-		log.Infof("unknown episode count for %v", p.url)
+		p.log.Infof("unknown episode count for %v", p.url)
 		return 0
 	}
 
-	log.Errorf("failed to find episode count for %v", p.url)
+	p.log.Warnf("failed to find episode count for %v", p.url)
 	return 0
 }
 
 func (p *Parser) Episodes() []*scraper.Episode {
 	eps := make([]*scraper.Episode, 0)
-	p.doc.Find(`table#eplist tr[itemprop="episode"]`).Each(func(_ int, s *goquery.Selection) {
+	p.doc.Find(`table#eplist tr[itemprop="episode"]`).Each(func(i int, s *goquery.Selection) {
+		log := p.log.WithField("ep_idx", i)
 		ep := new(scraper.Episode)
 		ep.Type = parseEpisodeType(s)
 		if ep.Type == scraper.Episode_UNKNOWN {
-			log.Warnf("unknown episode type: %v", s.Text())
+			log.Warnf("unknown episode type, ep name: %v", ep.Name)
 			return
 		}
 
-		ep.Number = parseEpisodeNumber(s)
-		ep.Name = parseEpisodeName(s)
-		ep.Duration = parseEpisodeDuration(s)
-		ep.AirDate = parseEpisodeDate(s)
+		number, err := parseEpisodeNumber(s)
+		if err != nil {
+			log.Warnf("failed to parse ep number: %v", err)
+		}
+
+		name, err := parseEpisodeName(s)
+		if err != nil {
+			log.Warnf("failed to parse ep name: %v", err)
+		}
+
+		duration, err := parseEpisodeDuration(s)
+		if err != nil {
+			log.Warnf("failed to parse ep duration: %v", err)
+		}
+
+		date, err := parseEpisodeDate(s)
+		if err != nil {
+			log.Warnf("failed to parse ep air date: %v", err)
+		}
+
+		ep.Number = number
+		ep.Name = name
+		ep.Duration = duration
+		ep.AirDate = date
 
 		eps = append(eps, ep)
 	})
@@ -169,10 +218,10 @@ func (p *Parser) Episodes() []*scraper.Episode {
 }
 
 func (p *Parser) StartDate() int64 {
-	raw := p.doc.Find(`"div.g_definitionlist tr.year span[itemprop="startDate"]"`).First().AttrOr("content", "")
+	raw := p.doc.Find(`div.g_definitionlist tr.year span[itemprop="startDate"]`).First().AttrOr("content", "")
 	d, err := parseDate(raw)
 	if err != nil {
-		log.Errorf("failed to parse start date: %v", err)
+		p.log.Warnf("failed to parse start date: %v", err)
 		return 0
 	}
 
@@ -180,10 +229,10 @@ func (p *Parser) StartDate() int64 {
 }
 
 func (p *Parser) EndDate() int64 {
-	raw := p.doc.Find(`"div.g_definitionlist tr.year span[itemprop="endDate"]"`).First().AttrOr("content", "")
+	raw := p.doc.Find(`div.g_definitionlist tr.year span[itemprop="endDate"]`).First().AttrOr("content", "")
 	d, err := parseDate(raw)
 	if err != nil {
-		log.Errorf("failed to parse end date: %v", err)
+		p.log.Warnf("failed to parse end date: %v", err)
 		return 0
 	}
 
@@ -194,8 +243,14 @@ func (p *Parser) Tags() []*scraper.Anime_Tag {
 	tags := make([]*scraper.Anime_Tag, 0)
 	p.doc.Find("div.g_definitionlist tr.tags span.g_tag").Each(func(_ int, s *goquery.Selection) {
 		tag := new(scraper.Anime_Tag)
-		name, id := parseTagName(s), parseTagID(s)
-		if len(name) == 0 || id < 0 {
+		name := parseTagName(s)
+		if len(name) == 0 {
+			return
+		}
+
+		id, err := parseTagID(s)
+		if err != nil {
+			p.log.Warnf(" failed to parse tag id: %v", err)
 			return
 		}
 
@@ -215,7 +270,7 @@ func (p *Parser) Rating() float64 {
 
 	r, err := strconv.ParseFloat(raw, 64)
 	if err != nil {
-		log.Errorf("failed to parse rating: %v", err)
+		p.log.Warnf("failed to parse rating: %v", err)
 		return 0
 	}
 
@@ -248,12 +303,12 @@ func (p *Parser) Description() string {
 func parseSource(str string, sep string) (int32, error) {
 	raw := strings.Split(str, sep)
 	if len(raw) == 0 {
-		return 0, &Error{fmt.Sprintf("'%v'empty after splitting at %v", str, sep)}
+		return 0, &Error{fmt.Sprintf("'%v' is not a source", str)}
 	}
 
 	s, err := strconv.Atoi(raw[len(raw) - 1])
 	if err != nil {
-		return 0, &Error{fmt.Sprintf("source is not an int: %v", s)}
+		return 0, &Error{fmt.Sprintf("not an int: %v", s)}
 	}
 
 	return int32(s), nil
@@ -275,67 +330,60 @@ func parseEpisodeType(s *goquery.Selection) scraper.Episode_Type {
 	}
 }
 
-func parseEpisodeNumber(s *goquery.Selection) int32 {
+func parseEpisodeNumber(s *goquery.Selection) (int32, error) {
 	raw := s.Find("td abbr").First().Text()
 	match := regexp.MustCompile(`\d+`).FindStringSubmatch(raw)
-	if len(match) != 1 {
-		log.Errorf("multiple episode numbers found: %v", raw)
-		return -1
+	if len(match) == 0 {
+		return 0, &Error{fmt.Sprintf("not found for: %v", raw)}
 	}
 
 	num, err := strconv.Atoi(match[0])
 	if err != nil {
-		log.Errorf("episode number is not an int: %v", err)
-		return -1
+		return 0, &Error{fmt.Sprintf("not an int: %v", err)}
 	}
 
-	return int32(num)
+	return int32(num), nil
 }
 
-func parseEpisodeName(s *goquery.Selection) string {
-	raw := s.Find("td.name").First().Text()
+func parseEpisodeName(s *goquery.Selection) (string, error) {
+	raw := s.Find(`td.name label[itemprop="name"]`).First().Text()
 
 	// generic name like "Episode 1" should be skipped
 	if regexp.MustCompile(`episode\s+[\d.]+`).MatchString(strings.ToLower(raw)) {
-		log.Infof("unnamed episode: %v", s)
-		return ""
+		return "", &Error{fmt.Sprintf("unnamed episode: %v", s)}
 	}
 
-	return strings.TrimSpace(raw)
+	return strings.TrimSpace(raw), nil
 }
 
-func parseEpisodeDuration(s *goquery.Selection) float64 {
+func parseEpisodeDuration(s *goquery.Selection) (float64, error) {
 	raw := s.Find("td.duration").First().Text()
 	raw = strings.TrimSpace(raw)
 	if len(raw) == 0 {
-		log.Warnf("episode duration not found: %v", s)
-		return 0
+		return 0, &Error{fmt.Sprintf("not found: %v", s)}
 	}
 
 	match := regexp.MustCompile(`(\d+)\s*m`).FindStringSubmatch(raw)
 	if len(match) == 0 {
-		log.Warnf("episode duration not found: %v", raw)
-		return 0
+		return 0, &Error{fmt.Sprintf("not found: %v", raw)}
 	}
 
-	mins, err := strconv.ParseFloat(match[0], 64)
+	mins, err := strconv.ParseFloat(match[1], 64)
 	if err != nil {
-		log.Errorf("failed to parse episode duration: %v", err)
-		return 0
+		return 0, &Error{fmt.Sprintf("parsing failed: %v", err)}
 	}
 
-	return mins * 60
+	return mins * 60, nil
 }
 
-func parseEpisodeDate(s *goquery.Selection) int64 {
+func parseEpisodeDate(s *goquery.Selection) (int64, error) {
 	raw := s.Find("td.airdate").First().AttrOr("content", "")
 	d, err := parseDate(raw)
 	if err != nil {
-		log.Errorf("%v: %v", err, s)
-		return 0
+		return 0, &Error{fmt.Sprintf("parsing failed: %v", err)}
 	}
 
-	return d
+	return d, nil
 }
 
 func parseDate(s string) (int64, error) {
@@ -368,19 +416,17 @@ func parseTagInfo(s *goquery.Selection) string {
 	return raw
 }
 
-func parseTagID(s *goquery.Selection) int32 {
+func parseTagID(s *goquery.Selection) (int32, error) {
 	raw := s.Find("a.tooltip").First().AttrOr("href", "")
 	match := regexp.MustCompile(`tagid=(\d+)`).FindStringSubmatch(raw)
-	if len(match) != 1 {
-		log.Errorf("multiple tag ids found: %v", match)
-		return -1
+	if len(match) == 0 {
+		return 0, &Error{fmt.Sprintf("not tag id: %v", raw)}
 	}
 
-	id, err := strconv.Atoi(match[0])
+	id, err := strconv.Atoi(match[1])
 	if err != nil {
-		log.Errorf("failed to parse tag id: %v", err)
-		return -1
+		return 0, &Error{fmt.Sprintf("parsing failed: %v", err)}
 	}
 
-	return int32(id)
+	return int32(id), nil
 }
