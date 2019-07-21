1c8e78f68097b9b8fcf7174ea7686e7b32ebd183
Igor Nikitin <rabbitinspace@icloud.com>
2019-07-21 21:16:43 +1000

    launch grpc server

--- spider/server.go
+++ spider/server.go
@@ -2,6 +2,9 @@ package spider
 
 import (
 	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
 
 	"satelit-project/satelit-scraper/proto/scraper"
 
@@ -9,6 +12,22 @@ import (
 )
 
 type scraperServiceServer struct{}
+
+func Serve(port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+
+	grpcServer := grpc.NewServer()
+	scraper.RegisterScraperServiceServer(grpcServer, &scraperServiceServer{})
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (s *scraperServiceServer) StartScraping(ctx context.Context, intent *scraper.ScrapeIntent) (*empty.Empty, error) {
 	err := RunScraper(ctx, intent)
