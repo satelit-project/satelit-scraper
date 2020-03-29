a08493b7595ab6a63c12c0210b423f321824670c
Igor Nikitin <rabbitinspace@icloud.com>
2020-03-29 15:29:27 +1100

    pretty print uuid

---
+++ proto/common/uuid.ext.go
@@ -0,0 +1,19 @@
+package common
+
+import "encoding/hex"
+
+func (u *UUID) String() string {
+	uuid := u.Uuid
+	var buf [36]byte
+	hex.Encode(buf[:], uuid[:4])
+	buf[8] = '-'
+	hex.Encode(buf[9:13], uuid[4:6])
+	buf[13] = '-'
+	hex.Encode(buf[14:18], uuid[6:8])
+	buf[18] = '-'
+	hex.Encode(buf[19:23], uuid[8:10])
+	buf[23] = '-'
+	hex.Encode(buf[24:], uuid[10:])
+
+	return string(buf[:])
+}
